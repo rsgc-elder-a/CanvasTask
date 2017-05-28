0ebaa211790299138ffa02fbcf7d0c0c18ecb03b
Andrew <aelder@rsgc.on.ca>
2017-05-28 16:36:19 -0400

    Got the data parsed and printing out now need to put it in my struct.

--- Animation/FileDataHolder.swift
+++ Animation/FileDataHolder.swift
@@ -17,7 +17,7 @@ public class DataHolder {
         var description : String
         var angleValue : Float
         var axiom : String
-        var rules : Rules
+        var rules : [Rules]
     }
     
     
@@ -63,6 +63,79 @@ public class DataHolder {
                 }
                 
             }
+            if (singleValues.contains("axiom")) { //detects when if find angle in a word
+                var firstVal = singleValues
+                var secondVal = ""
+                var finalInt:Float
+                var splitVal = firstVal.components(separatedBy: ":")
+                for values in splitVal {
+                    if values.contains("\n"){
+                        //print(values.components(separatedBy: "\n"))
+                        
+                        secondVal = values.components(separatedBy: "\n")[0]
+                        print(secondVal)
+                    }
+                }
+            }
+            if (singleValues.contains("=")) { //detects when if find angle in a word
+                var firstVal = singleValues
+                var pred = ""
+                var prob = 1
+                var secc = ""
+                var finalInt:Float
+                var splitVal = firstVal.components(separatedBy: "=")
+                pred = splitVal[0]
+                secc = splitVal[1]
+                if secc.contains("/") {
+                    var splitNow = secc.components(separatedBy: "/")
+                    if let probablity = Int(splitNow[0])  {
+                        prob = probablity
+                        //print(prob)
+                        
+                    } else {
+                        
+                    }
+                    secc = splitNow[1]
+                }
+                print(pred)
+                print(prob)
+                print(secc)
+                
+            }
+            if (singleValues.contains("generations")) { //detects when if find angle in a word
+                var firstVal = singleValues
+                var secondVal = ""
+                var finalInt:Int
+                var splitVal = firstVal.components(separatedBy: ":")
+                for values in splitVal {
+                    if values.contains("\n"){
+                        //print(values.components(separatedBy: "\n"))
+                        
+                        secondVal = values.components(separatedBy: "\n")[0]
+                        
+                        if let finalIntValue = Int(secondVal)  {
+                            finalInt = finalIntValue
+                            print("Gen: \(finalInt)")
+                            
+                        } else {
+                            
+                        }
+                        
+                        //guard let newColor = system.colors["2"] else {
+                        //   return
+                        //}
+                        
+                    }
+                }
+                
+            }
+
+
+            
+            
+            
+            
+            
         }
     }
 }
