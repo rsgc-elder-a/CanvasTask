f3fcb92512c540bd8d56d4645d39f54bd3baafc8
Andrew <aelder@rsgc.on.ca>
2017-05-28 16:42:15 -0400

    Got all data needed parsed but need to put it into data structures.

--- Animation/FileDataHolder.swift
+++ Animation/FileDataHolder.swift
@@ -121,6 +121,8 @@ public class DataHolder {
                             
                         }
                         
+                        
+                        
                         //guard let newColor = system.colors["2"] else {
                         //   return
                         //}
